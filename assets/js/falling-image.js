5ecc69ad0a9804dd4e35140ae468d96397af9b34
Ray <rberkele@ucsd.edu>
2024-03-21 21:33:21 -0700

    updated hover photos

--- assets/js/falling-image.js
+++ assets/js/falling-image.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const container = document.querySelector('.falling-image-container');
-    const imageUrl = 'assets/img/members/danse-baby.gif';
-    const numImages = 20; // Number of falling images
+  const container = document.querySelector('.falling-image-container');
+  const imageUrl = 'assets/img/members/danse-baby.gif';
+  const numImages = 20;
 
-    for (let i = 0; i < numImages; i++) {
-      const img = document.createElement('img');
-      img.src = imageUrl;
-      img.className = 'falling-image';
-      img.style.left = `${Math.random() * 100}%`;
-      img.style.animationDelay = `${Math.random() * 5}s`;
-      container.appendChild(img);
-    }
-  });
+  for (let i = 0; i < numImages; i++) {
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.className = 'falling-image';
+    img.style.left = `${Math.random() * 100}%`;
+    img.style.animationDelay = `${Math.random() * 5}s`;
+    container.appendChild(img);
+  }
+});
